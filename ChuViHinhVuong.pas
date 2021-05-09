856ffa4a9c0db1952a0a18067bd4fa454af54025
chupper100 <phamminhdat223@gmail.com>
2021-05-09 10:47:12 +0700

    Add fle

---
+++ ChuViHinhVuong.pas
@@ -0,0 +1,18 @@
+program ChuVivaDienTichHinhVuong;
+var
+	a: integer;
+	P: integer;
+	S: integer;
+
+begin
+	write('Nhap canh a cua hinh vuong: ');
+	readln(a);
+          P:=a*4;
+          S:=a*a;
+          write('Chu vi cua hinh vuong la: ', P);
+          readln;
+          write('Dien tich cua hinh vuong la: ', S);
+          readln;
+          write('pls insert a number');
+          readln;
+end.
